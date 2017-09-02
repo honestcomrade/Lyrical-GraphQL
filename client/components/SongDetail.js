031fb52f3bc5e1ef12ca3fff20f909dd3be66cb9
Joe Saraceno <honestcomrade@gmail.com>
2017-09-02 13:22:32 -0700

    detail route

---
+++ client/components/SongDetail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
+
+class SongDetail extends Component {
+  render() {
+    return(
+      <div>
+        <h3>
+          Song Detail
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default SongDetail;
