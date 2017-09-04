beb981d9864b826cc0456acbf62afa6deeb4ce83
Joe Saraceno <honestcomrade@gmail.com>
2017-09-04 10:44:38 -0700

    optimisitc response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -4,9 +4,17 @@ import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   
-  onLike(id) {
+  onLike(id, likes) {
     this.props.mutate({
-      variables: { id }
+      variables: { id },
+      optimisticResponse: { 
+        __typename: 'Mutation',
+        likeLyric : {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
     });
   }
 
@@ -18,7 +26,7 @@ class LyricList extends Component {
           <div className="vote-box">
             <i 
               className="material-icons"
-              onClick={ () => this.onLike(id) }
+              onClick={ () => this.onLike(id, likes) }
             >
               thumb_up
             </i>
